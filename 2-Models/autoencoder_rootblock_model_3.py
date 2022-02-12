092a31e94717612ea9de581868dd805ab2da43bb
conniaren <conniaren@hotmail.com>
2022-02-11 20:53:09 -0500

    Update script, can parse through the CSV file now

--- 2-Models/autoencoder_rootblock_model_3.py
+++ 2-Models/autoencoder_rootblock_model_3.py
@@ -9,11 +9,12 @@ import pytorch_lightning as pl
 import torch.nn.functional as F
 import vcf_data_loader
 import pandas as pd
+import sys
 
 #Function to add the softmax of two overlapping blocks together
 def concat_softmax (first_block, second_block):
   x12 = torch.concat((first_block,second_block), dim = 1)
-  x12.reshape(-1,2,3,1000).sum(axis=1)
+  x12.view(-1,2,3,1000).sum(axis=1)
   return x12
 
 if __name__ == "__main__":
@@ -24,7 +25,7 @@ if __name__ == "__main__":
     error = []
     
     #predictions = torch.tensor(pd.read_csv('./model_predictions.csv')) This causes an out of memory kill
-
+    csv.field_size_limit(sys.maxsize)
     #Open and read the csv file of model outputs
     f = open('./model_predictions.csv', 'r')
     reader = csv.reader(f)
@@ -40,49 +41,61 @@ if __name__ == "__main__":
 
         #Get the next line of the csv file
         line = next(reader)
-        print(len(line))
 
-        # Attempting to parse the csv file (This is where the script crashes) into a list of floats
+        # Attempting to parse the csv file into a list of floats
+        tensors = []
         for sample in line: 
             new_list = []
-            sample = sample[2:len(sample)-2]
-            for i in sample.split(","):
-                new_list.append(float(i))
-            print(len(new_list))
-       # predictions = torch.tensor(float(z) for z in line2)
+            sample = sample[2:-2] #remove list square brackets
+            for index, output in enumerate(sample.split(",")):
+                if index== 1999 or index == 3999 : 
+                    output = output[:-1] # remove closing square bracket
+                elif index == 2000 or index == 4000: 
+                    output = output[2:] # remove whitespace and opening square bracket
+                new_list.append(float(output))
+            tensors.append(torch.tensor(new_list).reshape(1,6000))
+            
+        predictions = torch.cat(tensors, dim=0)
 
         # Calculating accuracy and error for each block's output 
         # Note there is a different process (no need for concat_softmax() call) f
         #or blocks that do not need to be combined (first and last block)
+        losses = []
         if i == 0: 
-            x1 = predictions[:,:,0:1000]
-            previous_block = predictions[:,:,1000:2000]
-            loss = F.nll_loss(x1, dataset.to(int))
-            error.append(loss)
+            x1 = predictions[:,0:3000].view(-1, 3, 1000)
+            previous_block = predictions[:,3000:6000].view(-1, 3, 1000)
+            for position in range(1000):
+                loss = F.nll_loss(x1[:,:,position], dataset[:,position].to(int))
+                losses.append(loss)
+            mean_loss =  sum(losses)/len(losses)
+            error.append(mean_loss)
             accuracy = (dataset == x1.argmax(dim=1)).to(float).mean(dim=1).mean()
             accuracies.append(accuracy)
         else:
-            x1 = predictions[:,:,0:1000]
+            x1 = predictions[:,0:3000].view(-1, 3, 1000)
             aggregated_predictions = concat_softmax(previous_block,x1)
-            previous_block = predictions[:,:,1000:2000]
-            loss = F.nll_loss(aggregated_predictions, dataset.to(int))
-            error.append(loss)
+            previous_block = predictions[:,3000:6000].view(-1, 3, 1000)
+            for position in range(1000):
+                loss = F.nll_loss(aggregated_predictions[:,:,position], dataset[:,position].to(int))
+                losses.append(loss)
+            mean_loss =  sum(losses)/len(losses)
+            error.append(mean_loss)
             accuracy = (dataset == aggregated_predictions.argmax(dim=1)).to(float).mean(dim=1).mean()
             accuracies.append(accuracy)
         if i == 933:
-            x2 = predictions[:,:,1000:2000]
-            loss = F.nll_loss(x2, dataset2.to(int))
-            error.append(loss)
-            accuracy = (dataset2 == x2.argmax(dim=1)).to(float).mean(dim=1).mean()
+            x2 = predictions[:,3000:6000].view(-1, 3, 1000)
+            for position in range(1000):
+                loss = F.nll_loss(x2[:,:,position], dataset[:,position].to(int))
+                losses.append(loss)
+            mean_loss =  sum(losses)/len(losses)
+            error.append(mean_loss)
+            accuracy = (dataset == x2.argmax(dim=1)).to(float).mean(dim=1).mean()
             accuracies.append(accuracy)
+        print(f"Error at the {i} block: {mean_loss}")
+        print (f"Accuracy at the {i} block: {accuracy}")
         
-
-    print(error)
-    print (accuracy)
-    
-
-    print("AVERAGE ACCURACY:" + sum(accuracies)/len(accuracies))
-    print("AVERAGE ERROR:"+ sum(error)/len(error))
+    print(f"AVERAGE ACCURACY: {sum(accuracies)/len(accuracies)}")
+    print(f"AVERAGE ERROR: {sum(error)/len(error)}")
 
     f.close()
 
